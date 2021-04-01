6cd48e876972530d32c2b840b26a70e091823161
AsyaAndKo <asya.kosareva666@gmail.com>
2021-04-01 20:53:25 +0300

    Game of life playground

---
+++ hello.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    """Adds to numerics
+
+    Args:
+        a ([int]): first a
+        b ([int]): second a
+
+    Returns:
+        [int]: a+b
+    """
+    a = a**2
+    return a+b
